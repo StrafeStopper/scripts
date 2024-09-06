c372f05037c7ed1f140590129d3b1b1ca713e62e
StrafeStopper <strafestopper@gmail.com>
2024-09-05 22:24:23 -0600

    updated comments

--- snap-link/snap-link.sh
+++ snap-link/snap-link.sh
@@ -1,4 +1,5 @@
 # run this when you install new snap packages
 # zsh somehow broke snap PATH linking
 # this will copy snap links into the main applications directory
+# you need to run this as root (sudo)
 cp -a /var/lib/snapd/desktop/applications/. /usr/share/applications/
